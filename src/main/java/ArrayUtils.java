8066bcb65d8754fce25012aa1c8c5bc69f0d6df7
Elena Kote <elenakote@elenas-mbp.acasmart.jh.edu>
2024-04-01 13:31:56 -0400

    first ci-hello world

---
+++ src/main/java/ArrayUtils.java
@@ -0,0 +1,37 @@
+public class ArrayUtils {
+    /**
+     * Find last index of element
+     *
+     * @param x array to search
+     * @param y value to look for
+     * @return last index of y in x; -1 if absent
+     * @throws NullPointerException if x is null
+     */
+    // test: x = [2, 3, 5]; y = 2; Expected = 0
+    public static int findLast (int[] x, int y) {
+        for (int i=x.length-1; i >= 0; i--) {
+            if (x[i] == y) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Count odd or positive elements
+     *
+     * @param x array to search
+     * @return count of odd/positive values in x
+     * @throws NullPointerException if x is null
+     */
+    // test: x = [-3, -2, 0, 1, 4]; Expected = 3
+    public static int oddOrPos(int[] x) {
+        int count = 0;
+        for (int j : x) {
+            if (j % 2 != 0 || j > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
